abb3d8108b082fc46be2ec9bfd7891f36c1a8699
Djudax <Djudax122@gmail.com>
2025-11-16 22:30:22 +0200

    fix: add sass, ignore deprecation warnings

---
+++ vue-lottery-app/src/components/AppNavigation.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm mb-4">
+    <div class="container-fluid">
+      <div class="navbar-nav mx-auto">
+        <router-link to="/" class="nav-link">Home</router-link>
+        <router-link to="/about" class="nav-link">About</router-link>
+        <router-link to="/lottery" class="nav-link">Lottery</router-link>
+        <template v-if="!authStore.isAuthenticated">
+          <router-link to="/login" class="nav-link">Login</router-link>
+        </template>
+        <button v-else @click="authStore.logout" class="btn btn-outline-danger btn-sm ms-2">
+          Logout
+        </button>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script setup lang="ts">
+import { useAuthStore } from '@/stores/auth'
+const authStore = useAuthStore()
+</script>
--- vue-lottery-app/src/main.ts
+++ vue-lottery-app/src/main.ts
@@ -1,7 +1,7 @@
-import './assets/main.css'
-
+// src/main.ts
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import './styles/main.scss'
 
 import App from './App.vue'
 import router from './router'
